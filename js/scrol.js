d11b29e8b167e52b18e7e0b510545ebe7ba12462
Andrey <dron2323.and@yandex.ru>
2024-04-27 15:57:37 +0300

    add scroll

---
+++ js/scrol.js
@@ -0,0 +1,12 @@
+const horizontScroll = document.querySelector('.popular-destination__cards');
+const leftBtn = document.querySelector('.button__left');
+const rightBtn = document.querySelector('.button__right');
+
+rightBtn.addEventListener('click', () => {
+    horizontScroll.style.scrollBehavior = 'smooth';
+    horizontScroll.scrollLeft += 320;
+});
+
+leftBtn.addEventListener('click', () => {
+    horizontScroll.scrollLeft -= 320;
+});
