e448f0219891a0c42ffecced7963ba5097058bcb
Andrey <dron2323.and@yandex.ru>
2024-04-27 16:05:09 +0300

    added changes to the file  READMI.md

--- js/scrol.js
+++ js/scrol.js
@@ -1,6 +1,6 @@
 const horizontScroll = document.querySelector('.popular-destination__cards');
 const leftBtn = document.querySelector('.button__left');
-const rightBtn = document.querySelector('.button__right');
+const rightBtn = document.querySelectorgit ('.button__right');
 
 rightBtn.addEventListener('click', () => {
     horizontScroll.style.scrollBehavior = 'smooth';
